5209c9c0ab7aebdb2ceb4b45bcb18eac5deb28bf
edyta0106 <edyta0106@gmail.com>
2023-01-14 15:13:10 -0700

    set up file structure in my-app and created routes for all files. added css styling to header and about me

---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+
+export default function Header2() {
+  useEffect(() => {
+    window.scroll(function () {
+      if (document.scrollTop() > 50) {
+        document.querySelector(".nav").addClass("affix");
+        console.log("OK");
+      } else {
+        document.querySelector(".nav").removeClass("affix");
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <nav className="nav">
+        <div className="container">
+          <div className="logo">
+            <a href="/">Edyta Radomska</a>
+          </div>
+          <div id="mainListDiv" className="main_list">
+            <ul className="navlinks">
+              <li>
+                <Link to="/">About Me</Link>
+              </li>
+              <li>
+                <Link to="/portfolio">Portfolio</Link>
+              </li>
+              <li>
+                <Link to="/resume">Resume</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Me</Link>
+              </li>
+            </ul>
+          </div>
+          <span className="navTrigger">
+            <i></i>
+            <i></i>
+            <i></i>
+          </span>
+        </div>
+      </nav>
+      <section class="home"></section>
+    </>
+  );
+}
---
+++ src/components/PageContainer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+
+export default function PageContainer() {
+  return (
+    <div className="PageContainer">
+      <Routes>
+        <Route exact path="/" element={<About />} />
+        <Route exact path="/portfolio" element={<Portfolio />} />
+        <Route exact path="/resume" element={<Resume />} />
+        <Route exact path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
