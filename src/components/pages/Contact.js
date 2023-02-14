b047059f5b8939f16a04c457a623ef8199c8056f
edyta0106 <edyta0106@gmail.com>
2023-02-14 15:23:35 -0700

    removed form from contacts page and added contact information instead

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,6 @@
 import React from "react";
-
-import { Container } from "@mui/material";
-
+import { Container, IconButton } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import "../App.css";
 
 export default function Contact() {
@@ -10,7 +9,11 @@ export default function Contact() {
       <h2 className="myH2 my-5 body">Contact Me</h2>
       <p className="myH3 body my-2">Email: edyta0106@gmail.com</p>
       <p className="myH3 body my-5">Phone: 720.775.7736</p>
-      <p className="myH3 body my-5">LinkedIn</p>
+      <div className="d-flex justify-content-center">
+        <IconButton className="my-3" aria-label="Linkedin.com" onClick={() => window.open("https://www.linkedin.com/in/edyta-r-a7768424a/")}>
+          <LinkedInIcon fontSize="large" />
+        </IconButton>
+      </div>
     </Container>
   );
 }
